4292c78ea4da773e79fa95b3aeb7b3cc09458073
Maxim Bondarchuk <valta91@mail.ru>
2016-12-02 21:26:54 +0200

    1.0

---
+++ AR project/Assets/Scripts/SimpleInputManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleInputManager : MonoBehaviour {
+
+    private RaycastHit hit;
+    private const string wearStudioURL = "http://wear-studio.com/";
+    private const string targetTag = "Target";
+
+    void Update () 
+    {
+        
+        #if UNITY_ANDROID 
+        {
+            if(Input.touchCount > 0)
+            {
+                if(Input.GetTouch(0).phase == TouchPhase.Began)
+                {
+                    Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+                    if(Physics.Raycast(ray, out hit))
+                    {
+                        if(hit.collider.tag == targetTag)
+                        {
+                            Application.OpenURL(wearStudioURL);
+                        }
+                    }
+                }
+
+            }
+        }
+
+        #elif UNITY_EDITOR
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if(Physics.Raycast(ray, out hit))
+                {
+                    if(hit.collider.tag == targetTag)
+                    {
+                        Application.OpenURL(wearStudioURL);
+                        Debug.Log("clicked");
+                    }
+                }
+            } 
+        }
+        #endif
+    }
+}
